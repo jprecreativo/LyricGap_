6726f538630a29c9717bae058844cc27c9350223
jprecreativo <jprecreativo@DESKTOP-EF90IDF>
2017-10-11 22:09:20 +0200

    - Initial commit.

---
+++ src/lyricgap_/LyricGap_.java
@@ -0,0 +1,18 @@
+package lyricgap_;
+
+/**
+ *
+ * @author jprecreativo
+ */
+
+public class LyricGap_ 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+}
